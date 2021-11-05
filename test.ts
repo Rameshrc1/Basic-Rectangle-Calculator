71832d23be17aae9002a06ba9933dd705795628d
Ramesh Samaraweera <90851929+Rameshrc1@users.noreply.github.com>
2021-11-05 20:48:20 +0530

    Add files via upload
    Create File Code

---
+++ test.ts
@@ -0,0 +1,123 @@
+//let sampleText: string  = "Hello World";
+
+//console.log(sampleText);
+
+//const abc: string = "Hi";
+
+//abc = "Bye"; 
+
+//let a: number = 239;
+//let b: number = 0b00010;
+//let c: number = 0o0001;
+//let d: number = 0x1001;
+
+//let x: boolean = true;
+//let y: boolean = false;
+
+
+//let x: string = "Hello";
+//x = 100;
+
+
+//let x: any = "Hello";
+//x = 100;
+
+
+//let x: unknown = 300;
+//x = "Hello";
+
+
+//let x: object = {name: 'Kamal', age: 30, city: "Gampaha" };
+
+//console.log(x);
+
+//console.table(x);
+
+//let firstName: string = "Nimal";
+//let lastName: string = "Perera";
+
+// // My name is Nimal Perea
+//let fullName: string = "My name is " + firstName + " " + lastName;
+
+//let fullName: string = `My name is ${firstName} ${lastName}`;
+
+//console.log(fullName);
+
+//let students: string[] = ["Nimal", "Kamal", "Sunil"];
+
+//console.log(students[2]);
+//console.log(students[10]); // Undefined
+
+
+//let studentTuple: [string, string, string] = ["Nimal", "Kamal", "Sunil"];
+
+//console.log(studentTuple[2]);
+//console.log(studentTuple[10]); // Error
+
+//enum Color{
+    // Red = "22",
+    // Green = "33",
+   // Blue = "44"
+ //}
+
+ //let x: Color = Color.Red;
+
+ //console.log(x); // 22
+
+
+
+// Name Functions
+ //function add(x, y){
+     //return x+y;
+ //}
+ //let x = add(5, 2);
+ //console.log(x);
+
+
+
+//Anonymus Functions
+ //let sub = function (x: number, y: number){
+   //  return x-y;
+ //}
+
+ //console.log(sub(5,2));
+
+
+ //class Student{
+    // public name: string;
+   //  public age: number;
+
+    // constructor(){
+
+     //console.log("student OB created")
+ //}
+//}
+
+ //let s1: Student = new Student();
+//s1.name = "Kamal";
+//s1.age = 20;
+
+ //let s2: Student = new Student();
+ //let s3: Student = new Student();
+
+ //console.table(s1)
+
+
+ //class Student{
+   // public name: string;
+    //public age: number;
+
+    //constructor(n: string, a: number ){
+      //  this.name = n;
+        //this.age = a;
+    //}
+//}
+
+//let s1: Student = new Student("Kamal", 20);
+//let s2: Student = new Student("Nimal", 30);
+//let s3: Student = new Student("Wimala", 40);
+
+//console.table(s1)
+//console.table(s2)
+//console.table(s3)
+
