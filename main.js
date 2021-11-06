1149e17ba8f2751f2fc817eeb5cdef4c7426f918
Ramesh Samaraweera <90851929+Rameshrc1@users.noreply.github.com>
2021-11-06 22:09:34 +0530

    Create JS File

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var rectangle_1 = require("./rectangle");
+var r1 = new rectangle_1.Rectangle(10, 5);
+console.log(r1.getArea());
+console.log(r1.getPerameter());
